e7d919aa56d940044fa33576ec2563d19fb0cb65
Caleb Mennen <cmennen@cisco.com>
2018-12-17 09:37:20 -0500

    re-initing

--- 21/a.py
+++ 21/a.py
@@ -1,6 +1,25 @@
 import pprint
+import re
+from tqdm import trange
 
 pp = pprint.PrettyPrinter(indent=4)
 
-file = 'x.txt'
-data = [_.strip() for _ in open(file).readlines()]
+
+def process_input():
+    file = 'x.txt'
+    data = [_.strip() for _ in open(file).readlines()]
+    return data
+
+
+def get_input():
+    data = process_input()
+
+    return data
+
+def main():
+    data = get_input()
+    print(data)
+
+
+if __name__ == '__main__':
+    main()
