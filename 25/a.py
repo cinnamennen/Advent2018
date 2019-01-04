d2f99b307c368776e2f90bb6e75a2952d5d7ba31
Caleb Mennen <cmennen@cisco.com>
2019-01-04 16:59:13 -0500

    25a

--- 25/a.py
+++ 25/a.py
@@ -1,24 +1,39 @@
 import pprint
 import re
+from itertools import combinations
+
+import networkx as nx
+from cinnamon_tools.point import Point
 from tqdm import trange
 
 pp = pprint.PrettyPrinter(indent=4)
 
 
 def process_input():
-    file = 'x.txt'
+    file = 'a.txt'
     data = [_.strip() for _ in open(file).readlines()]
     return data
 
 
 def get_input():
     data = process_input()
+    data = [Point(*map(int, d.split(','))) for d in data]
 
     return data
 
+
 def main():
     data = get_input()
-    print(data)
+
+    g = nx.Graph()
+
+    for p in data:
+        g.add_node(p)
+
+    for x, y in combinations(data, 2):
+        if x.distance_to(y) <= 3:
+            g.add_edge(x,y)
+    print(nx.number_connected_components(g))
 
 
 if __name__ == '__main__':
